2bbc690d9ee9581414f499240a0fe0d8e4600b08
Anirudh <anirudhece2020@gmail.com>
2023-08-03 23:51:08 +0530

    typo

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import Store from "./store/store";
+import Store from "./store/Store.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
+      <Provider store={Store}>
         <App />
       </Provider>
     </BrowserRouter>
