ec2d8c598cd0213742268c893e16ed6237a4531d
Anirudh <anirudhece2020@gmail.com>
2023-08-03 23:57:26 +0530

    .

--- src/store/slices/addAssignmentReducer.jsx
+++ src/store/slices/addAssignmentReducer.jsx
@@ -2,10 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 const addAssignment= createSlice({
     name:'addAssignment',
     initialState:{
-
+        assignments:[],
+        assignmentCount:0,
     },
     reducers:{
-
+        // add assignemt
+        // delete assignment
     }
 });
 export const{}=addAssignment.actions;
