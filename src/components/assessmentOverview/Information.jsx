08185ece6d5dffd991ccf45d2c9a29eec9ea717e
Anirudh <anirudhece2020@gmail.com>
2023-08-03 18:18:34 +0530

    information component created

---
+++ src/components/assessmentOverview/Information.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+function Information({ count, added, text }) {
+  return (
+    <>
+      <Box>
+        <Box sx={{display:'flex',alignItems:'center'}}>
+          {count && (
+            <Typography
+              sx={{
+                color: "var(--text-100, #1C4980)",
+                fontFeatureSettings: "'clig' off, 'liga' off",
+                fontFamily: "Inter",
+                fontSize: "20px",
+                fontStyle: "normal",
+                fontWeight: "700",
+                lineHeight: "140%",
+              }}
+            >
+              {count}
+            </Typography>
+          )}
+          {added && (
+            <Typography
+              sx={{
+                color: "var(--status-success, #05C165)",
+                fontFeatureSettings: "'clig' off, 'liga' off",
+                fontFamily: "Inter",
+                fontSize: "12px",
+                fontStyle: "normal",
+                fontWeight: "500",
+                lineHeight: "140%",
+                alignItems:'center',
+              }}
+            >
+              {added > 0 ? "+" : "-"}
+              {added}
+            </Typography>
+          )}
+        </Box>
+        <Box>
+          {text && (
+            <Typography
+              sx={{
+                color: "#1C4980",
+                fontFeatureSettings: "'clig' off, 'liga' off",
+                fontFamily: "Inter",
+                fontSize: "12px",
+                fontStyle: "normal",
+                fontWeight: "500",
+                lineHeight: "140%",
+              }}
+            >
+              {text}
+            </Typography>
+          )}
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default Information;
---
+++ src/store/slices/assessmentOverView.jsx
@@ -0,0 +1,32 @@
+export const data = {
+  totalAssessment: {
+    count: 98,
+  },
+  candidates: {
+    totalCandidates: {
+      count: '11,145',
+      added: 89,
+    },
+    whoAttempted: {
+      count: 1140,
+      added: 89,
+    },
+  },
+  candidatesSource: {
+    email: {
+      count: '11,000',
+      added: 89,
+    },
+    socialShare: {
+      count: 145,
+      added: 89,
+    },
+    uniqueLink: {
+      count: 145,
+      added: 89,
+    },
+  },
+  totalPurpose: {
+    count: 11,
+  },
+};
