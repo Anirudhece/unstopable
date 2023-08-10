9cdc092be6cc750e1fc9596d260330f472574f99
Anirudh <anirudhece2020@gmail.com>
2023-08-10 17:36:58 +0530

    border on hover

--- src/components/InsideList.jsx
+++ src/components/InsideList.jsx
@@ -1,33 +1,59 @@
-import React from "react";
-import { Box,ListItem,ListItemText,ListItemIcon } from "@mui/material";
+import React, { useState } from "react";
+import { Box, ListItem, ListItemText, ListItemIcon } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function InsideList(props) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovered(false);
+  };
+
+  const borderColor = isHovered ? "var(--secondary-1, #0073E6)" : "transparent";
+
   return (
-    <>
-      <ListItem sx={{ justifyContent: "center" }}>
-            <Link style={{ textDecoration: "none", color: "#0073E6" }} to={props.to}>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  padding: "13px",
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    color: "#1C4980",
-                  }}
-                >
-                  <props.icon />
-                </ListItemIcon>
-                <ListItemText primary={props.value} />
-              </Box>
-            </Link>
-          </ListItem>
-    </>
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <ListItem
+        sx={{
+          display: "flex",
+          width: "80%",
+          justifyContent: "center",
+          border: `1px solid ${borderColor}`,
+          borderRadius: "8px",
+        }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        <Link
+          style={{ textDecoration: "none", color: "#0073E6" }}
+          to={props.to}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              m: "13px",
+              
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                color: "#1C4980",
+              }}
+            >
+              <props.icon />
+            </ListItemIcon>
+            <ListItemText primary={props.value} />
+          </Box>
+        </Link>
+      </ListItem>
+    </Box>
   );
 }
 
