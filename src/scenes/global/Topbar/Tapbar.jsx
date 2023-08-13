ba988cd21b9988a253130a2a71733da0269c1557
Anirudh <anirudhece2020@gmail.com>
2023-08-13 22:14:09 +0530

    topbar updated

--- src/scenes/global/Topbar/Tapbar.jsx
+++ src/scenes/global/Topbar/Tapbar.jsx
@@ -1,63 +1,116 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
-import { Box, Typography, Divider } from "@mui/material";
+import { Box, Typography, IconButton, Divider } from "@mui/material";
 import { useLocation } from "react-router-dom";
-function Tapbar() {
-  const location = useLocation();
-  return (
-    <Box
-      display="flex"
-      justifyContent="space-between"
-      className="topbar-container"
-      mb={1}
-    >
-      <Box display="flex" className="left" sx={{ alignItems: "center" }}>
-        <Box p={1} className="component-name">
-          <Typography
-            sx={{
-              color: "var(--text-100, #1C4980)",
-              fontFeatureSettings: "'clig' off, 'liga' off",
-              fontFamily: "Inter",
-              fontSize: "20px",
-              fontStyle: "normal",
-              fontWeight: "600",
-              lineHeight: "140%",
-            }}
-          >
-            {location.pathname.replace(/\//g, "")}
-          </Typography>
-        </Box>
-        <Divider sx={{ ml: 3, mr: 3 }} orientation="vertical" flexItem />
+import SegmentOutlinedIcon from "@mui/icons-material/SegmentOutlined";
 
-        <Box>
-          <Typography
-            pt={2}
-            pb={2}
-            sx={{
-              color: "var(--secondary-1, #0073E6)",
-              fontFamily: "Inter",
-              fontSize: "14px",
-              fontStyle: "normal",
-              fontWeight: "600",
-              lineHeight: "normal",
-              borderBottom: "2px solid",
-            }}
-          >
-            <>
-              My {}
+function Topbar({ isSidebarCollapsed, onSidebarToggle }) {
+  const location = useLocation();
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        className="topbar-container"
+        mb={1}
+        mt={1}
+        sx={{
+          "@media (max-width: 900px)": {
+            ml: 1,
+            mr: 1,
+          },
+        }}
+      >
+        <Box display="flex" className="left" sx={{ alignItems: "center" }}>
+          {windowWidth <= 900 && (
+            <Box
+              sx={{
+                display: "flex",
+                width: "40px",
+                height: "40px",
+                padding: "10px",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "10px",
+                borderRadius: "60px",
+                background: "var(--secondary-5, #F2F8FE)",
+              }}
+            >
+              <IconButton>
+                <SegmentOutlinedIcon
+                  clickable={true}
+                  sx={{
+                    width: "30px",
+                    height: "30px",
+                  }}
+                />
+              </IconButton>
+            </Box>
+          )}
+          <Box p={1} className="component-name">
+            <Typography
+              sx={{
+                color: "var(--text-100, #1C4980)",
+                fontFeatureSettings: "'clig' off, 'liga' off",
+                fontFamily: "Inter",
+                fontSize: "20px",
+                fontStyle: "normal",
+                fontWeight: "600",
+                lineHeight: "140%",
+              }}
+            >
               {location.pathname.replace(/\//g, "")}
-            </>
-          </Typography>
+            </Typography>
+          </Box>
+          <Divider sx={{ ml: 3, mr: 3 }} orientation="vertical" flexItem />
+
+          <Box>
+            <Typography
+              pt={2}
+              pb={2}
+              sx={{
+                color: "var(--secondary-1, #0073E6)",
+                fontFamily: "Inter",
+                fontSize: "14px",
+                fontStyle: "normal",
+                fontWeight: "600",
+                lineHeight: "normal",
+                borderBottom: "2px solid",
+              }}
+            >
+              <>
+                My {}
+                {location.pathname.replace(/\//g, "")}
+              </>
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          className="right"
+          sx={{ display: "flex", alignItems: "center", color: "#1C4980" }}
+        >
+          {isSidebarCollapsed && (
+            <IconButton onClick={onSidebarToggle}>
+              <PhoneAndroidIcon />
+            </IconButton>
+          )}
         </Box>
       </Box>
-      <Box
-        className="right"
-        sx={{ display: "flex", alignItems: "center", color: "#1C4980" }}
-      >
-        <PhoneAndroidIcon />
-      </Box>
-    </Box>
+    </>
   );
 }
 
-export default Tapbar;
+export default Topbar;
